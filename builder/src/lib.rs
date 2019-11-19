a650400aac483977de24dbe19eff7c369dbac354
nooberfsh <nooberfsh@gmail.com>
2019-11-19 11:01:39 +0800

    impl builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,10 +6,8 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
-use syn::{
-    parse_macro_input, parse_quote, Data, DeriveInput, Field, Fields, GenericArgument, Ident, Lit,
-    LitStr, Meta, NestedMeta, PathArguments, Type,
-};
+use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Field, Fields, GenericArgument, Ident, Lit, LitStr, Meta, NestedMeta, PathArguments, Type};
+
 
 fn is_option(ty: &Type) -> bool {
     if let Type::Path(ref p) = ty {
@@ -27,7 +25,7 @@ fn wrap_option_ty(ty: Type) -> Type {
         ty
     } else {
         parse_quote! {
-            Option<#ty>
+            ::std::option::Option<#ty>
         }
     }
 }
@@ -49,6 +47,20 @@ fn extract_ty_from_option(ty: Type) -> Type {
     unreachable!()
 }
 
+fn extract_ty_from_vec(ty: Type) -> Type {
+    if let Type::Path(tp) = ty {
+        let seg = tp.path.segments.first().unwrap();
+        if let PathArguments::AngleBracketed(args) = &seg.arguments {
+            let generic_arg = args.args.first().unwrap();
+            if let GenericArgument::Type(new_ty) = generic_arg {
+                return new_ty.clone();
+            }
+        }
+    }
+    unreachable!()
+
+}
+
 fn wrap_option_field(mut f: Field) -> Field {
     let ty = wrap_option_ty(f.ty);
     f.ty = ty;
@@ -67,6 +79,7 @@ fn check_builder_attribute(f: &Field) -> Result<Option<String>, TokenStream> {
     //assert!(format!("{:?}", attr.path) == "builder");
 
     let meta = attr.parse_meta().unwrap();
+    let span = meta.span();
     if let Meta::List(l) = meta {
         if l.nested.len() == 1 {
             if let NestedMeta::Meta(Meta::NameValue(nv)) = l.nested.into_iter().next().unwrap() {
@@ -81,50 +94,10 @@ fn check_builder_attribute(f: &Field) -> Result<Option<String>, TokenStream> {
             }
         }
     }
-    let ret = quote_spanned! { attr.span() =>
-        compile_error!(r#"expected `builder2(each = "...")`"#);
+    let ret = quote_spanned! { span =>
+        compile_error!(r#"expected `builder(each = "...")`"#);
     };
     Err(ret.into())
-}
-
-#[proc_macro_derive(Builder2, attributes(builder2))]
-pub fn derive2(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-
-    let struct_name = input.ident;
-    let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
-
-    let fields = if let Data::Struct(s) = input.data {
-        if let Fields::Named(named) = s.fields {
-            named.named
-        } else {
-            panic!("only used to named fields")
-        }
-    } else {
-        panic!("only used to struct")
-    };
-
-    let mut ty2attr: HashMap<Ident, String> = HashMap::new();
-
-    for f in fields.clone() {
-        match check_builder_attribute(&f) {
-            Ok(Some(d)) => {
-                ty2attr.insert(f.ident.clone().unwrap(), d);
-            }
-            Ok(None) => {}
-            Err(e) => return e,
-        };
-    }
-
-    let option_fields = fields.clone().into_iter().map(wrap_option_field);
-
-    let ret = quote! {
-        pub struct #builder_name {
-            #(#option_fields, )*
-        }
-    };
-
-    ret.into()
 }
 
 #[proc_macro_derive(Builder, attributes(builder))]
@@ -144,31 +117,89 @@ pub fn derive(input: TokenStream) -> TokenStream {
         panic!("only used to struct")
     };
 
-    let option_fields = fields.clone().into_iter().map(wrap_option_field);
+    // get builder attributes
+    let mut ty2attr: HashMap<Ident, String> = HashMap::new();
+    for f in fields.clone() {
+        match check_builder_attribute(&f) {
+            Ok(Some(d)) => {
+                ty2attr.insert(f.ident.clone().unwrap(), d);
+            }
+            Ok(None) => {}
+            Err(e) => return e,
+        };
+    }
 
+    // generate builder fields
+    let option_fields = fields.clone().into_iter().map(|mut f| {
+        let new_ty = if ty2attr.contains_key(f.ident.as_ref().unwrap()) {
+            f.ty.clone()
+        } else {
+            wrap_option_ty(f.ty.clone())
+        };
+        f.attrs.clear();
+        f.ty = new_ty;
+        f
+    });
+
+
+    // generate builder setters
     let setters = fields.clone().into_iter().map(|f| {
         let name = f.ident.clone().unwrap();
-        let raw_ty = extract_ty_from_option(f.ty.clone());
-        quote! {
-            pub fn #name(&mut self, #name: #raw_ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        let old_ty = f.ty.clone();
+        if let Some(attr_name) = ty2attr.get(f.ident.as_ref().unwrap()) {
+            let raw_ty = extract_ty_from_vec(f.ty.clone());
+            let new_name = Ident::new(attr_name, Span::call_site());
+            if attr_name == &name.to_string() {
+                quote! {
+                    pub fn #name(&mut self, #name: #raw_ty) -> &mut Self {
+                        self.#name.push(#name);
+                        self
+                    }
+                }
+            } else {
+                quote! {
+                    pub fn #name(&mut self, #name: #old_ty) -> &mut Self {
+                        self.#name = #name;
+                        self
+                    }
+
+                    pub fn #new_name(&mut self, #new_name: #raw_ty) -> &mut Self {
+                        self.#name.push(#new_name);
+                        self
+                    }
+                }
+            }
+        } else {
+            let raw_ty = extract_ty_from_option(f.ty.clone());
+            quote! {
+                pub fn #name(&mut self, #name: #raw_ty) -> &mut Self {
+                    self.#name = ::std::option::Option::Some(#name);
+                    self
+                }
             }
         }
     });
 
+    // generate builder constructor
     let constructor = fields.clone().into_iter().map(|f| {
-        let name = f.ident.unwrap();
-        let err = LitStr::new(&format!("field {} not set", name), Span::call_site());
-        if is_option(&f.ty) {
+        let name = f.ident.clone().unwrap();
+        if ty2attr.contains_key(f.ident.as_ref().unwrap()) {
             quote! {
-                #name: self.#name.take()
+                #name: self.#name.clone()
             }
         } else {
-            quote! {
-                #name: self.#name.take().ok_or(#err)?
+            let err = LitStr::new(&format!("field {} not set", name), Span::call_site());
+            if is_option(&f.ty) {
+                quote! {
+                    #name: self.#name.take()
+                }
+            } else {
+                quote! {
+                    #name: self.#name.take().ok_or(#err)?
+                }
             }
         }
+
     });
 
     let ret = quote! {
@@ -184,9 +215,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl #builder_name {
-           #(#setters )*
+            #(#setters )*
 
-            pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+            pub fn build(&mut self) -> ::std::result::Result<#struct_name, ::std::boxed::Box<dyn ::std::error::Error>> {
                 Ok(#struct_name {
                     #(#constructor, )*
                 })
