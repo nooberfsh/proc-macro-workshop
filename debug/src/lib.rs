7506b95fb5a4de5cfc9032b820672101bd17f7eb
nooberfsh <nooberfsh@gmail.com>
2019-11-19 17:22:22 +0800

    impl test3 in debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident, Attribute, Type, LitStr};
+use syn::{parse_macro_input, DeriveInput, Data, Fields, Ident, Attribute, Type, LitStr, Meta, Lit};
 use quote::{quote, quote_spanned};
 use proc_macro2::Span;
 
@@ -10,6 +10,55 @@ struct NamedStructField {
     ident: Ident,
     attr: Option<Attribute>, // at most 1
     ty: Type
+}
+
+struct NamedStructFieldWithAttr {
+    ident: Ident,
+    fmt: Option<LitStr>,
+    ty: Type
+}
+
+fn gen_fmt_str(struct_name: &Ident, fields: &[NamedStructFieldWithAttr]) -> LitStr {
+    let data = fields.iter().map(|f| {
+        match &f.fmt {
+            Some(l) => format!("{}: {}", f.ident, l.value()),
+            None => format!("{}: {}", f.ident, "{:?}"),
+        }
+    }).collect::<Vec<_>>().join(", ");
+    let ret = format!("{} {{{{ {} }}}}", struct_name, data);
+    LitStr::new(&ret, Span::call_site())
+}
+
+fn extract_named_struct_fields_and_attr(input: &DeriveInput) -> Result<Vec<NamedStructFieldWithAttr>, TokenStream> {
+    let fields = extract_named_struct_fields(input)?;
+    let mut ret =  vec![];
+    for field in fields {
+        let lit = if let Some(attr) = &field.attr {
+            Some(extract_format(attr)?)
+        } else {
+            None
+        };
+        ret.push(NamedStructFieldWithAttr {
+            ident: field.ident,
+            fmt: lit,
+            ty: field.ty,
+        })
+    }
+    Ok(ret)
+}
+
+fn extract_format(attr: &Attribute) -> Result<LitStr, TokenStream> {
+    let meta = attr.parse_meta().map_err(|e|e.to_compile_error())?;
+    let span = attr.span();
+    if let Meta::NameValue(nv) = meta {
+        if let Lit::Str(lstr) = nv.lit {
+            return Ok(lstr)
+        }
+    }
+    let e = quote_spanned! { span =>
+        compile_error!("expected attribute with named value");
+    };
+    Err(e.into())
 }
 
 fn extract_named_struct_fields(input: &DeriveInput) -> Result<Vec<NamedStructField>, TokenStream> {
@@ -40,18 +89,15 @@ fn ss<T: ToString, I: IntoIterator<Item = T>>(i: I) -> impl Iterator<Item = LitS
 }
 
 fn derive_impl(input: &DeriveInput) -> Result<TokenStream, TokenStream> {
-    let fields = extract_named_struct_fields(input)?;
+    let fields = extract_named_struct_fields_and_attr(input)?;
     let field_idents = fields.iter().map(|f| &f.ident);
-    let field_names = ss(field_idents.clone());
     let struct_ident = &input.ident;
-    let struct_name = s(&struct_ident);
+    let format_str = gen_fmt_str(&struct_ident, &fields);
 
     let ret = quote! {
         impl ::std::fmt::Debug for #struct_ident {
             fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
-                fmt.debug_struct(#struct_name)
-                    #(.field(#field_names, &self.#field_idents))*
-                    .finish()
+                fmt.write_fmt(format_args!(#format_str  #(, self.#field_idents)*))
             }
         }
     };
@@ -59,7 +105,7 @@ fn derive_impl(input: &DeriveInput) -> Result<TokenStream, TokenStream> {
     Ok(ret.into())
 }
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
--- main.rs
+++ main.rs
@@ -11,9 +11,11 @@ fn main() {}
 use derive_debug::CustomDebug;
 
 #[derive(CustomDebug)]
-pub struct XxField {
+pub struct Field {
     name: &'static str,
+    #[debug = "0b{:08b}"]
     bitmask: u8,
 }
+
 
 
