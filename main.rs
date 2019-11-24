49a2d62a6448a55d6d8816428a9208657c72ee2d
nooberfsh <nooberfsh@gmail.com>
2019-11-24 22:50:33 +0800

    impl test1 and test2 in seq

--- main.rs
+++ main.rs
@@ -6,40 +6,13 @@
 // To run the code:
 //     $ cargo run
 
+
+
+
+use seq::seq;
+
+seq!(N in 0..4 {
+    compile_error!(concat!("error number ", stringify!(N)));
+});
+
 fn main() {}
-
-
-use derive_debug::CustomDebug;
-use std::fmt::Debug;
-use std::marker::PhantomData;
-
-//pub trait Trait {
-//    type Value;
-//}
-//
-//#[derive(CustomDebug)]
-//#[debug(bound = "T::Value: Debug")]
-//pub struct Field<T: Trait, F, G> {
-//    f: F,
-//    g: PhantomData<G>,
-//    bitmask: u8,
-//    values: Vec<T::Value>,
-//}
-
-
-
-
-pub trait Trait {
-    type Value;
-}
-
-#[derive(CustomDebug)]
-#[debug(bound = "T::Value: Debug")]
-pub struct Wrapper<T: Trait> {
-    field: Field<T>,
-}
-
-#[derive(CustomDebug)]
-struct Field<T: Trait> {
-    values: Vec<T::Value>,
-}
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,73 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use syn::*;
+use syn::parse::*;
+use proc_macro2::{TokenStream, TokenTree, Literal, Group};
+
+
+#[derive(Debug)]
+struct Seq {
+    ident: Ident,
+    start: usize,
+    end: usize,
+    content: TokenStream,
+}
+
+enum State {
+    Empty,
+    Ident(Ident),
+    IdentPound(Ident),
+    IdentPoundNum(Ident),
+    IdentPoundNumPound(Ident),
+}
+
+impl Seq {
+    fn expand(&self) -> TokenStream {
+        (self.start..self.end)
+            .map(|n| replace(n, self.ident.clone(), self.content.clone()))
+            .flatten()
+            .collect()
+    }
+
+}
+
+fn replace(n: usize, id: Ident, content: TokenStream) -> impl Iterator<Item = TokenTree> {
+    content
+        .clone()
+        .into_iter()
+        .map(move |tt| match tt {
+            TokenTree::Ident(i) if i == id=> TokenTree::Literal(Literal::usize_unsuffixed(n)),
+            TokenTree::Group(g) => {
+                let grouped = g.stream();
+                let delim = g.delimiter();
+                let replaced = replace(n, id.clone(), grouped).collect();
+                TokenTree::Group(Group::new(delim, replaced))
+            },
+            _ => tt,
+        })
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ident: Ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let s: LitInt = input.parse()?;
+        let start = s.base10_parse::<usize>()?;
+        input.parse::<Token![..]>()?;
+        let e: LitInt = input.parse()?;
+        let end = e.base10_parse::<usize>()?;
+        let c;
+        braced!(c in input);
+        let content : TokenStream = c.parse()?;
+        // TODO: check range is valid
+        Ok(Seq{ident, start, end, content})
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let seq: Seq = parse_macro_input!(input);
+    let ret = seq.expand();
+    println!("{}", ret);
+    ret.into()
 }
