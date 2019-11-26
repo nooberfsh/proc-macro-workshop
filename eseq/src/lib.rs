2b9dde3992ff28ed75ba1215c89c03881d1efb32
nooberfsh <nooberfsh@gmail.com>
2019-11-26 18:21:34 +0800

    add esaq crate

---
+++ eseq/src/lib.rs
@@ -0,0 +1,8 @@
+
+use proc_macro_hack::proc_macro_hack;
+
+/// Add one to an expression.
+///
+/// (Documentation goes here on the re-export, not in the other crate.)
+#[proc_macro_hack]
+pub use seq::eseq;
